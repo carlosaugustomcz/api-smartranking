a4b4db5f845c4b04c53fa27433e7e25bab178545
carlos.augustomcz <carlos.augustomcz@gmail.com>
2021-11-10 08:49:34 -0300

    Conexao falhando

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:hYT66sW6kBuqifKp@realmcluster.4toyy.mongodb.net/test?retryWrites=true&w=majority',
+      'mongodb+srv://admin:N6OZYfqbEnLLZ2LI@cluster0.mljtr.mongodb.net/test?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useCreateIndex: true,
